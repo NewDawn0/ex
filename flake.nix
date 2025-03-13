874ebe7ff8010a38992fffa927d9670e4aaec8d7
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 13:17:54 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,47 +1,39 @@
 {
-  description = "A wrapper around extracting common archive formats";
+  description = "Your awesome flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { nixpkgs, ... }@inputs:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system: nixpkgs.legacyPackages.${system};
-    in {
-      packages = eachSystem (system:
-        let
-          pkgs = mkPkgs system;
-          src = ./.;
-        in {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "ex";
-            version = "1.0.0";
-            inherit src;
-            propagatedBuildInputs = with pkgs; [
-              gnutar
-              gzip
-              libarchive
-              p7zip
-              unzip
-              zstd
-            ];
-            installPhase = "install -D -m 755 ${src}/ex -t $out/bin";
-            meta = {
-              description =
-                "A command-line wrapper for extracting common archive formats";
-              longDescription = ''
-                This tool wraps around popular archive extraction commands, providing a simple interface to extract files from formats like ZIP, TAR, and more.
-                It streamlines file extraction for most pouplar archive formats.
-              '';
-              homepage = "https://github.com/NewDawn0/ex";
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platforms = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      ex = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "ex";
+        version = "1.0.0";
+        src = ./.;
+        propagatedBuildInputs = with pkgs; [
+          gnutar
+          gzip
+          libarchive
+          p7zip
+          unzip
+          zstd
+        ];
+        installPhase = "install -Dm755 ex $out/bin/ex";
+        meta = {
+          description =
+            "Command-line wrapper to extract common archive formats";
+          longDescription = ''
+            This tool wraps around popular archive extraction commands, providing a simple interface to extract files from formats like ZIP, TAR, and more.
+            It streamlines file extraction for most pouplar archive formats.
+          '';
+          homepage = "https://github.com/NewDawn0/ex";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
